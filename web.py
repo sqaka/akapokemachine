ce06d14fb9a22313952094fa15a51b323143d84e
makachang <wwwcoins.info@gmail.com>
2019-09-04 23:56:15 +0900

    next6 commit

--- web.py
+++ web.py
@@ -16,7 +16,7 @@ app.config['DEBUG'] = True
 UPLOAD_FOLDER = './static/images/default/'
 
 # ルーティング。/にアクセス時
-@app.route('/')
+@app.route("/")
 def index():
     return render_template('index.html')
 

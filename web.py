1f18f0fc9ff15ae496579cf91ffa39dffeb6cd64
makachang <wwwcoins.info@gmail.com>
2019-09-05 19:27:54 +0900

    go templates

--- web.py
+++ web.py
@@ -40,5 +40,5 @@ def post():
 
 if __name__ == '__main__':
     app.debug = True
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0') # , port=port
